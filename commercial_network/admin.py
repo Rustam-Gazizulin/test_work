f6039d4991605d95e50888e626377b83cf34679d
rgazizulin174 <rgazizulin174@gmail.com>
2023-01-30 14:49:08 +0500

    кастомизировал Админ

--- commercial_network/admin.py
+++ commercial_network/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.http import urlencode
 
 from commercial_network.models import Contact, Products, Provider
 
@@ -7,5 +10,16 @@ admin.site.register(Products)
 
 
 @admin.register(Provider)
-class CityAdmin(admin.ModelAdmin):
+class ProviderAdmin(admin.ModelAdmin):
     search_fields = ("contact__city__icontains", )
+    # list_display = ("name", "credit", "view_provider_link")
+    #
+    # def view_provider_link(self, obj):
+    #     # count = obj.provider.count()
+    #     url = (
+    #         reverse("admin:commercial_network_provider_changelist")
+    #         + "?"
+    #         + urlencode({"contact__id": f"{obj.id}"})
+    #     )
+    #     return format_html('<a href="{}">{} Email</a>', url)
+    # view_provider_link.short_description = "Email"
